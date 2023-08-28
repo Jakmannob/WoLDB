0e623236cdcc422ef58c25f06a599433983363a4
Jakob Eigenmann <ga27hib@mytum.de>
2023-08-29 00:26:41 +0200

    Casting arguments directly when loaded

--- woldb.py
+++ woldb.py
@@ -6,10 +6,10 @@ from wakeonlan import send_magic_packet
 
 load_dotenv()
 discord_token = os.getenv('DISCORD_TOKEN')
-server_id = os.getenv('SERVER_ID')
+server_id = int(os.getenv('SERVER_ID'))
 mac = os.getenv('MAC')
 ip = os.getenv('IP')
-port = os.getenv('PORT')
+port = int(os.getenv('PORT'))
 
 
 intents = discord.Intents.default()
@@ -18,14 +18,14 @@ tree = discord.app_commands.CommandTree(client)
 
 @tree.command(name="wakeupserver",
     description="A command that will invoke Wake on LAN to wake up the server.",
-    guild=discord.Object(id=int(server_id)))
+    guild=discord.Object(id=server_id))
 async def wake_up_server(interaction):
-    send_magic_packet(mac, ip_address=ip, port=int(port))
+    send_magic_packet(mac, ip_address=ip, port=port)
     await interaction.response.send_message("Starting server! Please stand by.")
 
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=int(server_id)))
+    await tree.sync(guild=discord.Object(id=server_id))
 
 
 client.run(discord_token)
