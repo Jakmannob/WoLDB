8b9f366c36d0fcd9e3a3973e49907a7c2c6bd725
Jakob Eigenmann <ga27hib@mytum.de>
2023-08-29 00:00:07 +0200

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # WoLDB
-A Wake-on-Lan Discord Bot (WoLDB), that can be used to remotely wake up a server via discord.
+A Wake-on-Lan Discord Bot (WoLDB), that can be used to remotely wake up a server via Discord.
+
+## Setup
+
+First, set up a Discord application with bot, add it to your server and note down
+the bot token. You will need it for the `DISCORD_TOKEN` in the following step.
+Then create an environment file named `.env` with the content:
+
+```conf
+DISCORD_TOKEN=<your token>
+SERVER_ID=<your server id>
+MAC=<your target computers MAC address>
+IP=<your target computers IP address>
+PORT=<your target computers WoL port>
+```
+
+And run the python script.
+
+## Usage
+
+In the Discord server, type `/wakeupserver` in any channel. This will trigger a
+WoL of your computer.
---
+++ woldb.py
@@ -0,0 +1,31 @@
+import os
+import discord
+from dotenv import load_dotenv
+from wakeonlan import send_magic_packet
+
+
+load_dotenv()
+discord_token = os.getenv('DISCORD_TOKEN')
+server_id = os.getenv('SERVER_ID')
+mac = os.getenv('MAC')
+ip = os.getenv('IP')
+port = os.getenv('PORT')
+
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+
+@tree.command(name="wakeupserver",
+    description="A command that will invoke Wake on LAN to wake up the server.",
+    guild=discord.Object(id=server_id))
+async def wake_up_server(interaction):
+    send_magic_packet(mac, ip_address=ip, port=int(port))
+    await interaction.response.send_message("Starting server! Please stand by.")
+
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=server_id))
+
+
+client.run(discord_token)
